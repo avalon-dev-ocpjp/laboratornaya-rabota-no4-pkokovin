d3242a36392e2b1ff99973eef5f391663bdf3428
Pavel Kokovin <pkokovin@mail.ru>
2019-10-06 12:35:31 +0300

    Lab 4 done

--- src/ru/avalon/java/dev/ocpjp/labs/models/Commodity.java
+++ src/ru/avalon/java/dev/ocpjp/labs/models/Commodity.java
@@ -5,11 +5,21 @@ import ru.avalon.java.dev.ocpjp.labs.core.io.RandomFileReader;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Абстрактное представление о товаре.
  */
-public interface Commodity {
+public interface Commodity extends Comparable<Commodity>{
+    @Override
+    default int compareTo(Commodity o) {
+        if (this.getPrice() < o.getPrice()) return -1;
+        if (this.getPrice() > o.getPrice()) return 1;
+        return 0;
+    }
+
     /**
      * Возвращает код товара.
      *
@@ -108,16 +118,7 @@ public interface Commodity {
      * @return экземпляр типа {@link CommodityBuilder}
      */
     static CommodityBuilder builder() {
-        /*
-         * TODO(Студент): Реализовать метод 'builder()' типа 'Commodity'
-         * В рамках задачи потребуется создать реализацию
-         * интерфейса CommodityBuilder, что в свою очередь
-         * потребует создания реализации для интерфейса Commodity.
-         *
-         * Созданные реализации случше всего инкапсулировать
-         * на уровне пакета.
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return new CommodityImpl.CommodityBuilderImpl();
     }
 
     /**
@@ -132,11 +133,15 @@ public interface Commodity {
     static Collection<Commodity> random(int limit) throws IOException {
         try (RandomFileReader reader = RandomFileReader.fromSystemResource("resources/household.csv")) {
             /*
-             * TODO(Студент): Реализовать создание случайных объектов типа 'Commodity'
+             * Реализовано создание случайных объектов типа 'Commodity'
              * 1. Для создания коллекции следует использовать метод 'generate()' класса 'Stream'
              * 2. Для получения коллекции следует использовать метод 'collect()' класса 'Stream'
              */
-            throw new UnsupportedOperationException("Not implemented yet!");
+            return Stream.generate(reader::readLine)
+                    .limit(limit)
+                    .map(Commodity::valueOf)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
         }
     }
 
@@ -149,10 +154,47 @@ public interface Commodity {
      */
     static Commodity valueOf(String string) {
         /*
-         * TODO(Студент): реализовать метод 'parse()' класса 'Commodity'
+         * Реализован метод 'parse()' класса 'Commodity'
          * Реализация метода должна быть основана на формате
          * файла 'resources/household.csv'.
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        String[] commodityContents = string.split(";");
+        String code = commodityContents[0];
+        String vendorCode = commodityContents[1];
+        String name = commodityContents[2];
+        int resuide;
+        if (isInteger(commodityContents[3])) {
+            resuide = Integer.valueOf(commodityContents[3]);
+        } else return null;
+        double price;
+        if (isDouble(commodityContents[4])) {
+            price = Double.valueOf(commodityContents[4]);
+        } else return null;
+
+        return Commodity.builder()
+                .code(code)
+                .vendorCode(vendorCode)
+                .name(name)
+                .residue(resuide)
+                .price(price)
+                .build();
+    }
+    private static boolean isInteger(String val) {
+        boolean flag = true;
+        try {
+            Integer.parseInt(val);
+        } catch (NumberFormatException e) {
+            flag = false;
+        }
+        return flag;
+    }
+    private static boolean isDouble(String val) {
+        boolean flag = true;
+        try {
+            Double.parseDouble(val);
+        } catch (NumberFormatException e) {
+            flag = false;
+        }
+        return flag;
     }
 }
